1447f8b63dee381c718f9978d9eb45a77005c089
Suraj <surajaskmini@gmail.com>
2024-01-31 06:59:19 +0545

    Added the documentation

--- core/config.py
+++ core/config.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()   # loads the env variables from .env file to the current enn
 
 class Settings:
     DATABASE_NAME = os.getenv('database')
