9ef08999e62ea72667ab68ba109766e4744a1b7a
Suraj <surajaskmini@gmail.com>
2024-01-31 06:23:26 +0545

    Defined the models and relationships among them

--- db/models.py
+++ db/models.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from core.config import setting
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_jwt_extended import JWTManager
+from datetime import datetime
 
 app = Flask(__name__)   # Initialization of flask application
 # setting up the database uri for establishing connection
@@ -9,6 +10,36 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{setting.DATABASE_USER
 app.config['SECRET_KEY'] = setting.SECRET_KEY
 
 db = SQLAlchemy(app)    # Initializing SQLAlchemy for flask instance to setup database
+jwt = JWTManager(app)   # For APIs to be used by other frontends as well
+
+class User(db.Model):
+    userid = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    name = db.Column(db.String(100), nullable = False)
+    email = db.Column(db.String(100), nullable = False, unique = True)
+    membershipdate = db.Column(db.DateTime, default  = datetime.utcnow(), nullable = False)
+    borrowedbooks = db.relationship('BorrowedBooks', backref = 'user', lazy = True)
+
+class Book(db.Model):
+    bookid = db.Column(db.String(50), primary_key = True)
+    title = db.Column(db.String(255), nullable = False)
+    isbn = db.Column(db.String(50), nullable = False)
+    publisheddate = db.Column(db.Date, nullable = False)
+    genre = db.Column(db.String(150), nullable = False)
+    bookdetails = db.relationship('BookDetails', backref = 'book', uselist = False, lazy = True)
+
+class BookDetails(db.Model):
+    detailsid = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    bookid = db.Column(db.String(50), db.ForeignKey('book.bookid'), nullable = False, unique = True)
+    numberofpages = db.Column(db.Integer, nullable = False)
+    publisher = db.Column(db.String(255), nullable = False)
+    language = db.Column(db.String(50), nullable = False)
+
+class BorrowedBooks(db.Model):
+    sn = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    userid = db.Column(db.Integer, db.ForeignKey('user.userid'), nullable = False)
+    bookid = db.Column(db.String(50), db.ForeignKey('book.bookid'), nullable = False)
+    borrowdate = db.Column(db.Date, default = datetime.utcnow(), nullable = False)
+    returndate = db.Column(db.Date, nullable = True)
 
 
 # creating all the models in the database
